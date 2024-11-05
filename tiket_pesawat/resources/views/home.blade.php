319ee75f9d266082db52137c28bbc856abef1157
Theoo6 <tioramadhani200@gmail.com>
2024-11-05 13:40:20 +0700

    testing

---
+++ tiket_pesawat/resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('master.layout')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            ini adalah halaman home
+        </div>
+    </div>
+</div>
+@endsection
