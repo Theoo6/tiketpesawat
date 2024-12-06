0114cf1c561d36aaf5d95030469d2d8b4f4a504a
Theoo6 <tioramadhani200@gmail.com>
2024-12-06 14:25:01 +0700

    wadododo

--- tiket_pesawat/resources/views/pesan/tiket.blade.php
+++ tiket_pesawat/resources/views/pesan/tiket.blade.php
@@ -28,9 +28,10 @@
                             <img src="{{ asset('img/garuda.jpg') }}" class="card-img-top" alt="">
                             <div class="card-body">
                                 <h5 class="card-title text-dark"></h5>
+                                <h5 class="card-title text-dark">Garuda Indonesia</h5>
                                 <p class="card-text">Tanggal: 10 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Si Jalak Harupat</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Amsterdam</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -40,10 +41,10 @@
                         <div class="card match-card h-100 shadow-sm">
                             <img src="{{ asset('img/garuda.jpg') }}" class="card-img-top" alt="Arema FC vs Persija">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Arema FC vs Persija</h5>
+                                <h5 class="card-title text-dark">Garuda Indonesia</h5>
                                 <p class="card-text">Tanggal: 18 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kanjuruhan</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Tokyo (Narita)</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -53,10 +54,10 @@
                         <div class="card match-card h-100 shadow-sm">
                             <img src="{{ asset('img/garuda.jpg') }}" class="card-img-top" alt="Bali United vs Persebaya">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Bali United vs Persebaya</h5>
+                                <h5 class="card-title text-dark">Garuda Indonesia</h5>
                                 <p class="card-text">Tanggal: 20 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kapten I Wayan Dipta</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Sydney</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -67,10 +68,10 @@
                             <img src="{{ asset('img/AirAsia.jpeg') }}" class="card-img-top"
                                 alt="Persija Jakarta vs PSS Sleman">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Persija Jakarta vs PSS Sleman</h5>
+                                <h5 class="card-title text-dark">AirAsia</h5>
                                 <p class="card-text">Tanggal: 24 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Gelora Bung Karno</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Kuala Lumpur</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -80,10 +81,10 @@
                         <div class="card match-card h-100 shadow-sm">
                             <img src="{{ asset('img/AirAsia.jpeg') }}" class="card-img-top" alt="Arema FC vs Persebaya">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Arema FC vs Persebaya</h5>
+                                <h5 class="card-title text-dark">AirAsia</h5>
                                 <p class="card-text">Tanggal: 28 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kanjuruhan</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Surabaya - Singapore</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -93,10 +94,10 @@
                         <div class="card match-card h-100 shadow-sm">
                             <img src="{{ asset('img/AirAsia.jpeg') }}" class="card-img-top" alt="Persib Vs Persis">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Persib Vs Persis</h5>
+                                <h5 class="card-title text-dark">AirAsia</h5>
                                 <p class="card-text">Tanggal: 30 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kapten I Wayan Dipta</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Bangkok</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -107,10 +108,10 @@
                         <div class="card match-card h-100 shadow-sm mt-4">
                             <img src="{{ asset('img/lion.png') }}" class="card-img-top" alt="">
                             <div class="card-body">
-                                <h5 class="card-title text-dark"></h5>
+                                <h5 class="card-title text-dark">Lion Air</h5>
                                 <p class="card-text">Tanggal: 10 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Si Jalak Harupat</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Bali</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -120,10 +121,10 @@
                         <div class="card match-card h-100 shadow-sm mt-4">
                             <img src="{{ asset('img/lion.png') }}" class="card-img-top" alt="Arema FC vs Persija">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Arema FC vs Persija</h5>
+                                <h5 class="card-title text-dark">Lion Air</h5>
                                 <p class="card-text">Tanggal: 18 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kanjuruhan</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Surabaya</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -133,10 +134,10 @@
                         <div class="card match-card h-100 shadow-sm mt-4">
                             <img src="{{ asset('img/lion.png') }}" class="card-img-top" alt="Bali United vs Persebaya">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Bali United vs Persebaya</h5>
+                                <h5 class="card-title text-dark">Lion Air</h5>
                                 <p class="card-text">Tanggal: 20 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kapten I Wayan Dipta</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Bali - Makassar</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -147,10 +148,10 @@
                             <img src="{{ asset('img/citilink.jpg') }}" class="card-img-top"
                                 alt="Persija Jakarta vs PSS Sleman">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Persija Jakarta vs PSS Sleman</h5>
+                                <h5 class="card-title text-dark">Citilink Indonesia</h5>
                                 <p class="card-text">Tanggal: 24 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Gelora Bung Karno</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Surabaya</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -160,10 +161,10 @@
                         <div class="card match-card h-100 shadow-sm mt-4">
                             <img src="{{ asset('img/citilink.jpg') }}" class="card-img-top" alt="Arema FC vs Persebaya">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Arema FC vs Persebaya</h5>
+                                <h5 class="card-title text-dark">Citilink Indonesia</h5>
                                 <p class="card-text">Tanggal: 28 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kanjuruhan</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Bali</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -173,10 +174,10 @@
                         <div class="card match-card h-100 shadow-sm mt-4">
                             <img src="{{ asset('img/citilink.jpg') }}" class="card-img-top" alt="Persib Vs Persis">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Persib Vs Persis</h5>
+                                <h5 class="card-title text-dark">Citilink Indonesia</h5>
                                 <p class="card-text">Tanggal: 30 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kapten I Wayan Dipta</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Surabaya - Makassar</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
@@ -184,79 +185,79 @@
                 <div class="row row-cols-1 row-cols-md-3 g-4">
                     <!-- Pertandingan 1 -->
                     <div class="col">
-                        <div class="card match-card h-100 shadow-sm mt-4">
+                        <div class="card match-card h-100 shadow-sm mt-5">
                             <img src="{{ asset('img/sriwijaya.jpg') }}" class="card-img-top" alt="">
                             <div class="card-body">
-                                <h5 class="card-title text-dark"></h5>
+                                <h5 class="card-title text-dark">Sriwijaya Air</h5>
                                 <p class="card-text">Tanggal: 10 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Si Jalak Harupat</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Medan</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
 
                     <!-- Pertandingan 2 -->
                     <div class="col">
-                        <div class="card match-card h-100 shadow-sm mt-4">
+                        <div class="card match-card h-100 shadow-sm mt-5">
                             <img src="{{ asset('img/sriwijaya.jpg') }}" class="card-img-top" alt="">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Arema FC vs Persija</h5>
+                                <h5 class="card-title text-dark">Sriwijaya Air</h5>
                                 <p class="card-text">Tanggal: 18 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kanjuruhan</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Bali</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
 
                     <!-- Pertandingan 3 -->
                     <div class="col">
-                        <div class="card match-card h-100 shadow-sm mt-4">
+                        <div class="card match-card h-100 shadow-sm mt-5">
                             <img src="{{ asset('img/sriwijaya.jpg') }}" class="card-img-top" alt="Bali United vs Persebaya">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Bali United vs Persebaya</h5>
+                                <h5 class="card-title text-dark">Sriwijaya Air</h5>
                                 <p class="card-text">Tanggal: 20 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kapten I Wayan Dipta</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Palembang</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
 
                     <!-- Pertandingan 4 -->
                     <div class="col">
-                        <div class="card match-card h-100 shadow-sm mt-4">
+                        <div class="card match-card h-100 shadow-sm mt-5">
                             <img src="{{ asset('img/wing.jpeg') }}" class="card-img-top"
                                 alt="Persija Jakarta vs PSS Sleman">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Persija Jakarta vs PSS Sleman</h5>
+                                <h5 class="card-title text-dark">Wings Air</h5>
                                 <p class="card-text">Tanggal: 24 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Gelora Bung Karno</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Yogyakarta</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
 
                     <!-- Pertandingan 5 -->
                     <div class="col">
-                        <div class="card match-card h-100 shadow-sm mt-4">
+                        <div class="card match-card h-100 shadow-sm mt-5">
                             <img src="{{ asset('img/wing.jpeg') }}" class="card-img-top" alt="Arema FC vs Persebaya">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Arema FC vs Persebaya</h5>
+                                <h5 class="card-title text-dark">Wings Air</h5>
                                 <p class="card-text">Tanggal: 28 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kanjuruhan</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Jakarta - Surabaya</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
 
                     <!-- Pertandingan 6 -->
                     <div class="col">
-                        <div class="card match-card h-100 shadow-sm mt-4">
+                        <div class="card match-card h-100 shadow-sm mt-5">
                             <img src="{{ asset('img/wing.jpeg') }}" class="card-img-top" alt="Persib Vs Persis">
                             <div class="card-body">
-                                <h5 class="card-title text-primary">Persib Vs Persis</h5>
+                                <h5 class="card-title text-dark">Wings Air</h5>
                                 <p class="card-text">Tanggal: 30 Desember 2024</p>
-                                <p class="card-text">Lokasi: Stadion Kapten I Wayan Dipta</p>
-                                <a href="https://wa.me/083845035569" class="btn btn-outline-primary">Beli Tiket</a>
+                                <p class="card-text">Tujuan : Bali - Lombok</p>
+                                <a href="https://wa.me/085648554408" class="btn btn-outline-primary">Beli Tiket</a>
                             </div>
                         </div>
                     </div>
